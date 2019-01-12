1ac76f23012ad4abe229e3cc49e65744323501aa
陈立文 <1410762621@qq.com>
2019-01-12 13:46:00 +0800

    说明

--- http01.js
+++
@@ -1,9 +0,0 @@
-var http=require("http");
-var fs=require("fs");
-
-http.createServer(function (request,response) {
-    fs.readFile("./d.txt","utf-8",function (err,data) {
-        if (err) throw err;
-        response.end(data);
-    });
-}).listen(3000);
--- test.js
+++
@@ -1,9 +0,0 @@
-var m1=require("./module01");  //用require的方法来接收module01所暴露的接口并赋值给m1
-var m2=require("./module02");
-/*m1=={
-    "fn01":fn01
-    "fn02":fn02
-}
-* */
-m1.fn01();
-m2.fn02();
